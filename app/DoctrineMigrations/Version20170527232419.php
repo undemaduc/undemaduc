52d4318d02562cd535abb2b46edf872617b645eb
rolandkasa95 <rolandkasa95@gmail.com>
2017-05-28 02:51:58 +0300

    New: migrations.

---
+++ app/DoctrineMigrations/Version20170527232419.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170527232419 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE match_making (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, luser_id INT DEFAULT NULL, l_to_u TINYINT(1) DEFAULT \'0\' NOT NULL, u_to_l TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_831C28A4A76ED395 (user_id), INDEX IDX_831C28A4D8646676 (luser_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE match_making ADD CONSTRAINT FK_831C28A4A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE match_making ADD CONSTRAINT FK_831C28A4D8646676 FOREIGN KEY (luser_id) REFERENCES luser (id)');
+        $this->addSql('DROP TABLE `match`');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE `match` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, luser_id INT DEFAULT NULL, l_to_u TINYINT(1) DEFAULT \'0\' NOT NULL, u_to_l TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_7A5BC505A76ED395 (user_id), INDEX IDX_7A5BC505D8646676 (luser_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `match` ADD CONSTRAINT FK_7A5BC505A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE `match` ADD CONSTRAINT FK_7A5BC505D8646676 FOREIGN KEY (luser_id) REFERENCES luser (id)');
+        $this->addSql('DROP TABLE match_making');
+    }
+}
