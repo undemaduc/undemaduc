fd133e848645968e3cda753f7f1e0aba0438db97
Zoli Szogyenyi <zoltan.szogyenyi@gmail.com>
2017-05-27 20:57:26 +0300

    Somewhat finish dropdown.

--- web/src/components/UserLoginPage.js
+++ web/src/components/UserLoginPage.js
@@ -4,7 +4,20 @@ const UserLoginPage = () => {
   return (
       <div className="umd-setup-container umd-login-page">
 
-          <h1 className="setup__title">I'm <br /> <span className="underline">looking for</span> <br /> a place.</h1>
+          <h1 className="setup__title">I'm</h1>
+
+          <div className="dropdown umd-dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  looking for
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a className="dropdown-item" href="#">offering</a>
+              </div>
+          </div>
+
+          <h1 className="setup__title">a place.</h1>
+
+
 
       </div>
   );
