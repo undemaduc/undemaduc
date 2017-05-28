3177e556b8aa5bedd913a7a3e4815c1bae7e8573
Zoli Szogyenyi <zoltan.szogyenyi@gmail.com>
2017-05-28 04:11:52 +0300

    Animate events.

--- web/src/components/elements/Event.js
+++ web/src/components/elements/Event.js
@@ -9,7 +9,7 @@ class Event extends React.Component {
     render () {
 
         return (
-            <div className={"event " + (this.props.hide ? "hide" : "show")}>
+            <div className={"event hvr-float " + (this.props.hide ? "hide" : "show")}>
                 <img className="event__image" src={this.props.image} alt="Untold Festival"/>
                 <h3 className="event__title">{this.props.title}</h3>
                 <p className="event__date">{this.props.date}</p>
